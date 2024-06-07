7cd5698c0c1e355afdb4f1538229f5dc0294b3d8
KKH <gyooheon@naver.com>
2024-06-07 20:23:13 +0900

    ✏️ Fix : 디폹트 url 위치 수정

--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -3,7 +3,7 @@ import { getAccessToken } from './token';
 
 const accessToken = getAccessToken();
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_UTRIP_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${accessToken}`,
